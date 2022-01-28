d94dc52d8d6c7dbc69d2dd6573ddbf503afba761
cjvan <cjvan@DESKTOP-6EBFU6B>
2022-01-28 10:35:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Name: Cheyenne Van Helden
+// Assignment: Teach Assignment #2 - Basic Console App 
+
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One of my favorite quotes is: 'Be a voice, not an echo.'");
+        }
+    }
+}
